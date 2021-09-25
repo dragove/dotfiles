e7b4c4e8e0f49ab1de45f85244bbc7eb5f06d0ca
dragove <dragove@qq.com>
2021-09-25 13:22:29 +0800

    xmonad: support clicable workspace

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -16,14 +16,25 @@ myKeys = [ ((mod4Mask, xK_p), spawn "rofi -show combi")
          , ((0, xF86XK_MonBrightnessDown), spawn "brightnessControl.sh down")
 	 ]
 
+xmobarEscape = concatMap doubleLts
+  where doubleLts '<' = "<<"
+        doubleLts x   = [x]
+
+myWorkspaces = clickable . (map xmobarEscape) $ ["1:\xf120","2:\xf269","3:\xf121", "4:\xe217","5:\xe28b","6:\xf04b","7","8","9"]
+  where                                                                       
+         clickable l = [ "<action=xdotool key super+" ++ show (n) ++ ">" ++ ws ++ "</action>" |
+                            (i,ws) <- zip [1..9] l,
+                            let n = i ]
 
 main = do
-    h <- spawnPipe "xmobar"
+    xmproc <- spawnPipe "xmobar"
     xmonad $ docks $ ewmh def { 
 	      terminal        = "kitty"
 	    , modMask         = mod4Mask
-		  , logHook         = dynamicLogWithPP xmobarPP {ppOutput = hPutStrLn h}
-		  , handleEventHook = handleEventHook def <+> fullscreenEventHook
-	    , layoutHook      = smartBorders $ avoidStruts $ layoutHook def
-		} `additionalKeys` myKeys
+            , workspaces      = myWorkspaces
+            , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc }
+            , handleEventHook = handleEventHook def <+> fullscreenEventHook
+            , layoutHook      = smartBorders $ avoidStruts $ layoutHook def
+              } `additionalKeys` myKeys
 
