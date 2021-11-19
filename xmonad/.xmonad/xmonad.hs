b889a0ab484766a3baa20972621fd29ab35f45b8
dragove <dragove@qq.com>
2021-11-20 01:28:51 +0800

    update xmonad to 0.17 config

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,14 +1,37 @@
-import qualified Data.Map as M
-import Graphics.X11.ExtraTypes.XF86
 import XMonad
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.ManageDocks
-import XMonad.Layout.NoBorders
-import XMonad.Util.EZConfig (additionalKeys)
-import XMonad.Util.Run
 
-myKeys =
+import Graphics.X11.ExtraTypes.XF86
+
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
+
+import XMonad.Util.EZConfig
+import XMonad.Util.Loggers
+import XMonad.Util.Ungrab
+import XMonad.Util.ClickableWorkspaces
+
+import XMonad.Layout.Magnifier
+
+import XMonad.Hooks.EwmhDesktops
+
+
+main :: IO ()
+main = xmonad
+     . ewmhFullscreen
+     . ewmh
+     . withEasySB (statusBarProp "xmobar" (clickablePP myXmobarPP)) defToggleStrutsKey
+     $ myConfig
+
+myConfig = def
+    { terminal   = "kitty"
+    , modMask    = mod4Mask      -- Rebind Mod to the Super key
+    , layoutHook = myLayout      -- Use custom layouts
+    , manageHook = myManageHook  -- Match on certain windows
+    }
+  `additionalKeys`
   [ ((mod4Mask, xK_p), spawn "rofi -show combi"),
     ((mod4Mask, xK_s), spawn "flameshot gui"),
     ((0, xF86XK_AudioRaiseVolume), spawn "volumeControl.sh up"),
@@ -17,41 +40,44 @@ myKeys =
     ((0, xF86XK_MonBrightnessDown), spawn "brightnessControl.sh down")
   ]
 
-xmobarEscape = concatMap doubleLts
-  where
-    doubleLts '<' = "<<"
-    doubleLts x = [x]
-
-myWorkspaces =
-  clickable . (map xmobarEscape) $
-    [ "1:\xf120",
-      "2:\xf269",
-      "3:\xf121",
-      "4:\xe217",
-      "5:\xe28b",
-      "6:\xf04b",
-      "7",
-      "8",
-      "9"
+myManageHook :: ManageHook
+myManageHook = composeAll
+    [ className =? "Gimp" --> doFloat
+    , isDialog            --> doFloat
     ]
-  where
-    clickable l =
-      [ "<action=xdotool key super+" ++ show (n) ++ ">" ++ ws ++ "</action>"
-        | (i, ws) <- zip [1 .. 9] l,
-          let n = i
-      ]
 
-main = do
-  xmproc <- spawnPipe "xmobar"
-  xmonad $
-    docks $
-      ewmh
-        def
-          { terminal = "kitty",
-            modMask = mod4Mask,
-            workspaces = myWorkspaces,
-            logHook = dynamicLogWithPP xmobarPP {ppOutput = hPutStrLn xmproc},
-            handleEventHook = handleEventHook def <+> fullscreenEventHook,
-            layoutHook = smartBorders $ avoidStruts $ layoutHook def
-          }
-        `additionalKeys` myKeys
+myLayout = tiled ||| Mirror tiled ||| Full
+  where
+    tiled    = Tall nmaster delta ratio
+    nmaster  = 1      -- Default number of windows in the master pane
+    ratio    = 1/2    -- Default proportion of screen occupied by master pane
+    delta    = 3/100  -- Percent of screen to increment by when resizing panes
+
+myXmobarPP :: PP
+myXmobarPP = def
+    { ppSep             = magenta " • "
+    , ppTitleSanitize   = xmobarStrip
+    , ppCurrent         = wrap " " "" . xmobarBorder "Top" "#8be9fd" 2
+    , ppHidden          = white . wrap " " ""
+    , ppHiddenNoWindows = lowWhite . wrap " " ""
+    , ppUrgent          = red . wrap (yellow "!") (yellow "!")
+    , ppOrder           = \[ws, l, _, wins] -> [ws, l, wins]
+    , ppExtras          = [logTitles formatFocused formatUnfocused]
+    }
+  where
+    formatFocused   = wrap (white    "[") (white    "]") . magenta . ppWindow
+    formatUnfocused = wrap (lowWhite "[") (lowWhite "]") . blue    . ppWindow
+
+    -- | Windows should have *some* title, which should not not exceed a
+    -- sane length.
+    ppWindow :: String -> String
+    ppWindow = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 30
+
+    blue, lowWhite, magenta, red, white, yellow :: String -> String
+    magenta  = xmobarColor "#ff79c6" ""
+    blue     = xmobarColor "#bd93f9" ""
+    white    = xmobarColor "#f8f8f2" ""
+    yellow   = xmobarColor "#f1fa8c" ""
+    red      = xmobarColor "#ff5555" ""
+    lowWhite = xmobarColor "#bbbbbb" ""
+
