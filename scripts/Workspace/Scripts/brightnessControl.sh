4f72546c1ca1840612900def8fffb52d58724285
dragove <dragove@qq.com>
2022-04-03 14:21:41 +0800

    switch to sway

--- scripts/Workspace/Scripts/brightnessControl.sh
+++ scripts/Workspace/Scripts/brightnessControl.sh
@@ -9,14 +9,14 @@
 # https://gist.github.com/sebastiencs/5d7227f388d93374cebdf72e783fbd6a
 
 function get_brightness {
-  xbacklight -get | cut -d '.' -f 1
+  sudo light -G | cut -d '.' -f 1
 }
 
 function send_notification {
   icon="preferences-system-brightness-lock"
   brightness=$(get_brightness)
   # Make the bar with the special character ─ (it's not dash -)
-  # https://en.wikipedia.org/wiki/Box-drawing_character
+  # https://en.wikipediorg/wiki/Box-drawing_character
   bar=$(seq -s "─" 0 $((brightness / 5)) | sed 's/[0-9]//g')
   # Send the notification
   dunstify -i "$icon" -r 5555 -u normal "$bar"
@@ -25,12 +25,12 @@ function send_notification {
 case $1 in
   up)
     # increase the backlight by 5%
-    xbacklight -inc 5
+    sudo light -A 5
     send_notification
     ;;
   down)
     # decrease the backlight by 5%
-    xbacklight -dec 5
+    sudo light -U 5
     send_notification
     ;;
 esac
--- zsh/.zprofile
+++ zsh/.zprofile
@@ -1,8 +1,11 @@
 export PATH=$PATH:~/Workspace/Scripts:~/.local/bin
 export QT_QPA_PLATFORMTHEME=qt5ct
+export QT_QPA_PLATFORM=wayland
+export SDL_VIDEODRIVER=wayland
+export MOZ_ENABLE_WAYLAND=1
 export EDITOR=nvim
 export VISUAL=nvim
 if [[ ! $DISPLAY && $XDG_VTNR -eq 1 ]]; then
-  exec startx
+  exec sway --unsupported-gpu
 fi
 
