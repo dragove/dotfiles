2b43c5fc0c73011f89e8e11eaf402706eca36f3f
dragove <dragove@qq.com>
2023-01-15 22:01:40 +0800

    update astronvim keymapping

---
+++ astronvim/.config/astronvim/lua/user/mappings.lua
@@ -0,0 +1,9 @@
+return {
+  n = {
+    ["\\"] = { "<cmd>Neotree toggle<cr>", desc = "Open NeoTree" },
+    ["<C-\\>"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
+  },
+  t = {
+    ["<C-\\>"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
+  }
+}
