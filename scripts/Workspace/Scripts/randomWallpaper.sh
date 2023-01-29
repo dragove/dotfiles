6802abf78e81d5827f6bccf574bbcf5e4e3925c9
dragove <dragove@qq.com>
2023-01-30 00:52:13 +0800

    use new form of swww bin

--- scripts/Workspace/Scripts/randomWallpaper.sh
+++ scripts/Workspace/Scripts/randomWallpaper.sh
@@ -1,10 +1,10 @@
+swww-daemon &
 sleep 1
-swww init
 while true;
 do
   ls -d ~/Pictures/Wallpaper/* | sort -R | while read file;
   do
-    swww img "$file"
+    swww img "$file" --transition-step 4 --transition-fps 36 --transition-type random
     sleep 300
   done
 done
