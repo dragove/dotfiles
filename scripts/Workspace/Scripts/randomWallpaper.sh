81c8c7e9525a452142bdcbd7f91f57aca223a7cb
dragove <dragove@qq.com>
2023-03-15 00:15:13 +0800

    better swww smooth

--- scripts/Workspace/Scripts/randomWallpaper.sh
+++ scripts/Workspace/Scripts/randomWallpaper.sh
@@ -3,7 +3,7 @@ while true;
 do
   ls -d ~/Pictures/Wallpaper/* | sort -R | while read file;
   do
-    swww img "$file" --transition-step 4 --transition-fps 36 --transition-type random
+    swww img "$file" --transition-step 16 --transition-fps 30 --transition-type random
     sleep 300
   done
 done
