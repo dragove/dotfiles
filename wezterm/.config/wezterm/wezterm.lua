aa7481c7ef981fa46d248b9311e3435a50b673e5
dragove <dragove@qq.com>
2023-04-02 14:28:41 +0800

    switch color scheme to catppuccin

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -11,37 +11,5 @@ return {
         top = 4,
         bottom = 4,
     },
-    -- modified colors from OneHalfDark
-    colors = {
-        foreground = '#dcdfe4',
-        background = '#282c34',
-
-        cursor_bg = '#51afef',
-        cursor_fg = 'black',
-        cursor_border = '#a3b3cc',
-
-        selection_fg = '#dcdfe4',
-        selection_bg = '#474e5d',
-
-        ansi = {
-            '#282c34',
-            '#e06c75',
-            '#98c379',
-            '#e5c07b',
-            '#61afef',
-            '#c678dd',
-            '#56b6c2',
-            '#dcdfe4',
-        },
-        brights = {
-            '#595e68',
-            '#e06c75',
-            '#98c379',
-            '#e5c07b',
-            '#61afef',
-            '#c678dd',
-            '#56b6c2',
-            '#dcdfe4',
-        },
-    },
+    color_scheme = "Catppuccin Frappe",
 }
