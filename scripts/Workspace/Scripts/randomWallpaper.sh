267f5bc1ae86566407a4c3741b783c0706347e72
dragove <dragove@qq.com>
2023-04-15 10:40:22 +0800

    fix wallpaper change at startup

--- scripts/Workspace/Scripts/randomWallpaper.sh
+++ scripts/Workspace/Scripts/randomWallpaper.sh
@@ -1,9 +1,7 @@
 swww init
-while true;
-do
-  ls -d ~/Pictures/Wallpaper/* | sort -R | while read file;
-  do
-    swww img "$file" --transition-step 16 --transition-fps 30 --transition-type random
-    sleep 300
-  done
+while true; do
+	ls -d ~/Pictures/Wallpaper/* | sort -R | while read file; do
+		sleep 300
+		swww img "$file" --transition-step 16 --transition-fps 30 --transition-type random
+	done
 done
