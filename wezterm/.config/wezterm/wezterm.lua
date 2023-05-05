66b362ae3b4f22fd92c4651c821ff85b98f4691c
dragove <dragove@qq.com>
2023-05-05 21:13:27 +0800

    update

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 return {
     -- color_scheme = "OneHalfDark",
     font = wezterm.font("FiraCode Nerd Font"),
-    font_size = 18,
+    font_size = 20,
     hide_tab_bar_if_only_one_tab = true,
     -- window_background_opacity = 0.9,
     window_padding = {
