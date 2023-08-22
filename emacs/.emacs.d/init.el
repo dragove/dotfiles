7694722b60991d4023665b9e77a52fe63f66e4ff
jingchun <jingchun@tsign.cn>
2023-08-22 13:55:33 +0800

    corfu icons and beeter modeline color

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -216,11 +216,18 @@
 (use-package catppuccin-theme
   :config
   (setq catppuccin-flavor 'frappe)
-  (load-theme 'catppuccin t))
+  (load-theme 'catppuccin :no-confirm))
 
+(use-package nerd-icons)
 (use-package doom-modeline
   :ensure t
-  :init (doom-modeline-mode 1))
+  :after catppuccin-theme
+  :custom
+  (nerd-icons-color-icons nil)
+  :init
+  (set-face-background 'mode-line "base")
+  (set-face-background 'mode-line-inactive "base")
+  (doom-modeline-mode 1))
 
 (use-package ace-window
   :bind (("M-o" . ace-window)))
@@ -645,7 +652,7 @@
   ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
   ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
   ;; (corfu-scroll-margin 5)        ;; Use scroll margin
-
+  
   ;; Enable Corfu only for certain modes.
   ;; :hook ((prog-mode . corfu-mode)
   ;;        (shell-mode . corfu-mode)
@@ -674,6 +681,52 @@
   ;; Enable indentation+completion using the TAB key.
   ;; `completion-at-point' is often bound to M-TAB.
   (setq tab-always-indent 'complete))
+(use-package kind-icon
+  :ensure t
+  :after corfu
+  :custom
+  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
+  (kind-icon-use-icons nil)
+  (kind-icon-mapping
+   `(
+     (array ,(nerd-icons-codicon "nf-cod-symbol_array") :face font-lock-type-face)
+     (boolean ,(nerd-icons-codicon "nf-cod-symbol_boolean") :face font-lock-builtin-face)
+     (class ,(nerd-icons-codicon "nf-cod-symbol_class") :face font-lock-type-face)
+     (color ,(nerd-icons-codicon "nf-cod-symbol_color") :face success)
+     (command ,(nerd-icons-codicon "nf-cod-terminal") :face default)
+     (constant ,(nerd-icons-codicon "nf-cod-symbol_constant") :face font-lock-constant-face)
+     (constructor ,(nerd-icons-codicon "nf-cod-triangle_right") :face font-lock-function-name-face)
+     (enummember ,(nerd-icons-codicon "nf-cod-symbol_enum_member") :face font-lock-builtin-face)
+     (enum-member ,(nerd-icons-codicon "nf-cod-symbol_enum_member") :face font-lock-builtin-face)
+     (enum ,(nerd-icons-codicon "nf-cod-symbol_enum") :face font-lock-builtin-face)
+     (event ,(nerd-icons-codicon "nf-cod-symbol_event") :face font-lock-warning-face)
+     (field ,(nerd-icons-codicon "nf-cod-symbol_field") :face font-lock-variable-name-face)
+     (file ,(nerd-icons-codicon "nf-cod-symbol_file") :face font-lock-string-face)
+     (folder ,(nerd-icons-codicon "nf-cod-folder") :face font-lock-doc-face)
+     (interface ,(nerd-icons-codicon "nf-cod-symbol_interface") :face font-lock-type-face)
+     (keyword ,(nerd-icons-codicon "nf-cod-symbol_keyword") :face font-lock-keyword-face)
+     (macro ,(nerd-icons-codicon "nf-cod-symbol_misc") :face font-lock-keyword-face)
+     (magic ,(nerd-icons-codicon "nf-cod-wand") :face font-lock-builtin-face)
+     (method ,(nerd-icons-codicon "nf-cod-symbol_method") :face font-lock-function-name-face)
+     (function ,(nerd-icons-codicon "nf-cod-symbol_method") :face font-lock-function-name-face)
+     (module ,(nerd-icons-codicon "nf-cod-file_submodule") :face font-lock-preprocessor-face)
+     (numeric ,(nerd-icons-codicon "nf-cod-symbol_numeric") :face font-lock-builtin-face)
+     (operator ,(nerd-icons-codicon "nf-cod-symbol_operator") :face font-lock-comment-delimiter-face)
+     (param ,(nerd-icons-codicon "nf-cod-symbol_parameter") :face default)
+     (property ,(nerd-icons-codicon "nf-cod-symbol_property") :face font-lock-variable-name-face)
+     (reference ,(nerd-icons-codicon "nf-cod-references") :face font-lock-variable-name-face)
+     (snippet ,(nerd-icons-codicon "nf-cod-symbol_snippet") :face font-lock-string-face)
+     (string ,(nerd-icons-codicon "nf-cod-symbol_string") :face font-lock-string-face)
+     (struct ,(nerd-icons-codicon "nf-cod-symbol_structure") :face font-lock-variable-name-face)
+     (text ,(nerd-icons-codicon "nf-cod-text_size") :face font-lock-doc-face)
+     (typeparameter ,(nerd-icons-codicon "nf-cod-list_unordered") :face font-lock-type-face)
+     (type-parameter ,(nerd-icons-codicon "nf-cod-list_unordered") :face font-lock-type-face)
+     (unit ,(nerd-icons-codicon "nf-cod-symbol_ruler") :face font-lock-constant-face)
+     (value ,(nerd-icons-codicon "nf-cod-symbol_field") :face font-lock-builtin-face)
+     (variable ,(nerd-icons-codicon "nf-cod-symbol_variable") :face font-lock-variable-name-face)
+     (t ,(nerd-icons-codicon "nf-cod-code") :face font-lock-warning-face)))
+  :config
+  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
 
 ;; Add extensions
 (use-package cape
