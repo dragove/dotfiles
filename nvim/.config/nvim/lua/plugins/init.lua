61da99d65fe7cb1424835df3392db1670c90e0b6
dragove <dragove@qq.com>
2023-10-03 16:22:11 +0800

    simplify language configs

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,5 +1,6 @@
 return {
 	{ import = "lazyvim.plugins.extras.test.core" },
+	{ import = "lazyvim.plugins.extras.dap.core" },
 	{ import = "plugins.disabled" },
 	{ import = "plugins.ui" },
 	{ import = "plugins.code" },
---
+++ nvim/.config/nvim/lua/plugins/lang/core.lua
@@ -0,0 +1,5 @@
+return {
+	{ import = "lazyvim.plugins.extras.lang.python" },
+	{ import = "lazyvim.plugins.extras.lang.tailwind" },
+	{ import = "lazyvim.plugins.extras.lang.typescript" },
+}
