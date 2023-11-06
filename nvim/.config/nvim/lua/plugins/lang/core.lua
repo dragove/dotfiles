ef13287995f091dde22dc8d6c8e41e65d25f2a56
dragove <dragove@qq.com>
2023-11-07 01:02:22 +0800

    update

--- nvim/.config/nvim/lua/plugins/lang/core.lua
+++ nvim/.config/nvim/lua/plugins/lang/core.lua
@@ -1,6 +1,5 @@
 return {
 	{ "neovim/nvim-lspconfig", opts = { inlay_hints = { enabled = true } } },
-	{ import = "lazyvim.plugins.extras.lang.python" },
 	{ import = "lazyvim.plugins.extras.lang.tailwind" },
 	{ import = "lazyvim.plugins.extras.lang.typescript" },
 	{ import = "lazyvim.plugins.extras.lang.typescript" },
