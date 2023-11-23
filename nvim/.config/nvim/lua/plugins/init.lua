2a9fdb8c0206aeb7eb10eaa50deac8a040d18a69
dragove <dragove@qq.com>
2023-11-23 23:12:25 +0800

    update emacs config and add rust to nvim config

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,6 +1,7 @@
 return {
 	{ import = "lazyvim.plugins.extras.test.core" },
 	{ import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
 	{ import = "plugins.disabled" },
 	{ import = "plugins.ui" },
 	{ import = "plugins.code" },
