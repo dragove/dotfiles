78fb26404fdda18eea825d3f18976c5b4de808ee
dragove <dragove@qq.com>
2023-12-22 18:13:11 +0800

    feat: add support for markdown

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -838,6 +838,21 @@
                  (:exclude ".dir-locals.el" "*-tests.el")))
   :commands (eat))
 
+(use-package markdown-mode
+  :ensure t
+  :mode ("README\\.md\\'" . gfm-mode)
+  :init (setq markdown-command "multimarkdown")
+  :custom
+  (markdown-enable-math t)
+  (markdown-enable-wiki-links t)
+  (markdown-italic-underscore t)
+  (markdown-asymmetric-header t)
+  (markdown-make-gfm-checkboxes-buttons t)
+  (markdown-gfm-uppercase-checkbox t)
+  (markdown-fontify-whole-heading-line t)
+  (markdown-fontify-code-blocks-natively t)
+  (markdown-enable-hilighting-syntax t))
+
 (use-package treesit
   :elpaca nil
   :custom
