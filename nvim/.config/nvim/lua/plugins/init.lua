35064cb12e1bd9521e4485894cc875394e4e2439
dragove <dragove@qq.com>
2024-03-06 00:31:17 +0800

    use lua instead of json

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -2,6 +2,7 @@ return {
 	{ import = "lazyvim.plugins.extras.test.core" },
 	{ import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.clangd" },
 	{ import = "plugins.disabled" },
 	{ import = "plugins.ui" },
 	{ import = "plugins.code" },
