e569dbf736e12fd3b843e06565f5b40c74e9647c
dragove <dragove@qq.com>
2024-03-31 22:44:12 +0800

    feat: add jdtls

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -5,6 +5,7 @@ return {
 	{ import = "lazyvim.plugins.extras.lang.python" },
 	{ import = "lazyvim.plugins.extras.lang.clangd" },
 	{ import = "lazyvim.plugins.extras.lang.cmake" },
+	{ import = "lazyvim.plugins.extras.lang.java" },
 	{ import = "plugins.disabled" },
 	{ import = "plugins.ui" },
 	{ import = "plugins.code" },
