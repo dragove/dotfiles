fa84fb82527ebc61a1ea7b74b7d1bb4f881c9d39
dragove <dragove@qq.com>
2024-04-26 17:49:32 +0800

    feat: goto ref without declaration

--- nvim/.config/nvim/lua/config/mappings.lua
+++ nvim/.config/nvim/lua/config/mappings.lua
@@ -1,27 +1,14 @@
--- Helper function
-local default_opts = {
-	noremap = true,
-	silent = true,
-	expr = false,
-	nowait = false,
-	script = false,
-	unique = false,
-}
+local keymap = vim.keymap.set
 
-local keymap = function(mode, keys, cmd, opts)
-	local o = vim.tbl_deep_extend("force", default_opts, opts or {})
-	vim.keymap.set(mode, keys, cmd, o)
-end
-
--- NOTE: Most mappings come from 'mini.basics'
-
--- Shorter version of the most frequent way of going outside of terminal window
-keymap("t", [[<C-h>]], [[<C-\><C-N><C-w>h]])
+local silent = { silent = true }
 
 -- Paste before/after linewise
-vim.keymap.set({ "n", "x" }, "[p", '<Cmd>exe "put! " . v:register<CR>', { desc = "Paste Above" })
-vim.keymap.set({ "n", "x" }, "]p", '<Cmd>exe "put "  . v:register<CR>', { desc = "Paste Below" })
+keymap({ "n", "x" }, "[p", '<Cmd>exe "put! " . v:register<CR>', { desc = "Paste Above" })
+keymap({ "n", "x" }, "]p", '<Cmd>exe "put "  . v:register<CR>', { desc = "Paste Below" })
 
 -- navigate buffers
-vim.keymap.set({ "n", "x" }, "H", "<Cmd>bp<CR>", { desc = "previous buffer" })
-vim.keymap.set({ "n", "x" }, "L", "<Cmd>bn<CR>", { desc = "next buffer" })
+keymap({ "n", "x" }, "H", "<Cmd>bp<CR>", { desc = "previous buffer" })
+keymap({ "n", "x" }, "L", "<Cmd>bn<CR>", { desc = "next buffer" })
+
+-- lsp
+keymap("n", "gr", "<cmd>Telescope lsp_references include_declaration=false<cr>")
