2fc67442fa4fc2a4f6617ca71697522892f1353c
金雄镕 <dragove@qq.com>
2024-04-26 14:14:04 +0800

    Update wezterm.lua

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,16 +1,37 @@
 local wezterm = require("wezterm")
 return {
-    front_end = "WebGpu",
-    -- color_scheme = "OneHalfDark",
-    font = wezterm.font("FiraCode Nerd Font"),
-    font_size = 20,
-    hide_tab_bar_if_only_one_tab = true,
-    -- window_background_opacity = 0.9,
-    window_padding = {
-        left = 4,
-        right = 4,
-        top = 4,
-        bottom = 4,
-    },
-    color_scheme = "Catppuccin Frappe",
+	front_end = "WebGpu",
+	font = wezterm.font("FiraCode Nerd Font"),
+	font_size = 16,
+	hide_tab_bar_if_only_one_tab = true,
+	window_decorations = "INTEGRATED_BUTTONS|RESIZE",
+	window_padding = {
+		left = 4,
+		right = 4,
+		top = 4,
+		bottom = 4,
+	},
+	use_fancy_tab_bar = false,
+	window_frame = {
+		inactive_titlebar_bg = "#303446",
+		active_titlebar_bg = "#303446",
+	},
+	colors = {
+		tab_bar = {
+			-- The color of the inactive tab bar edge/divider
+            background = "#303446",
+			active_tab = {
+                bg_color = "#303446",
+                fg_color = "#f0f0f0"
+            },
+            inactive_tab = {
+                bg_color = "#303446",
+                fg_color = "#999999"
+            }
+		},
+	},
+    show_new_tab_button_in_tab_bar = false,
+	color_scheme = "Catppuccin Frappe",
+	-- for windows
+	-- default_prog = { "nu.exe" },
 }
