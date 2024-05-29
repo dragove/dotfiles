cf847aad49cbc81321c6356fe50e0508a5ce590b
jingchun <jingchun@tsign.cn>
2024-05-29 10:37:08 +0800

    nvim: add <A-o> map to jump window

--- nvim/.config/nvim/lua/config/mappings.lua
+++ nvim/.config/nvim/lua/config/mappings.lua
@@ -1,7 +1,5 @@
 local keymap = vim.keymap.set
 
-local silent = { silent = true }
-
 -- Paste before/after linewise
 keymap({ "n", "x" }, "[p", '<Cmd>exe "put! " . v:register<CR>', { desc = "Paste Above" })
 keymap({ "n", "x" }, "]p", '<Cmd>exe "put "  . v:register<CR>', { desc = "Paste Below" })
@@ -9,6 +7,11 @@ keymap({ "n", "x" }, "]p", '<Cmd>exe "put "  . v:register<CR>', { desc = "Paste 
 -- navigate buffers
 keymap({ "n", "x" }, "H", "<Cmd>bp<CR>", { desc = "previous buffer" })
 keymap({ "n", "x" }, "L", "<Cmd>bn<CR>", { desc = "next buffer" })
+
+-- jump window
+keymap({ "n", "i", "t" }, "<A-o>", function()
+	vim.cmd.wincmd("w")
+end, { desc = "jump to another window" })
 
 -- lsp
 keymap("n", "gr", "<cmd>Telescope lsp_references include_declaration=false<cr>")
