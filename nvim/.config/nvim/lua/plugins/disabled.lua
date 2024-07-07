a66018226251226670d82f681bd21f48362c582f
dragove <dragove@qq.com>
2024-07-07 12:29:42 +0800

    remove miniindent

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,6 +1,5 @@
 return {
 	{ "folke/tokyonight.nvim", enabled = false },
 	{ "nvim-lualine/lualine.nvim", enabled = false },
-	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
-	{ "echasnovski/mini.indentscope", enabled = false },
+	{ "nvim-neo-tree/neo-tree.nvim", enabled = false }
 }
