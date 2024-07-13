db500ca3024c1744a53e454058329b005df0009b
dragove <dragove@qq.com>
2024-07-13 22:44:10 +0800

    nvim: add key map in insert mode

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -5,5 +5,12 @@ map({ "n", "i", "t" }, "<A-o>", function()
 	vim.cmd.wincmd("w")
 end, { desc = "jump to another window" })
 
+map({ "i" }, "<C-f>", "<Right>")
+map({ "i" }, "<C-b>", "<Left>")
+map({ "i" }, "<C-a>", "<Home>")
+map({ "i" }, "<C-e>", "<End>")
+map({ "i" }, "<C-v>", "<PageDown>")
+map({ "i" }, "<M-v>", "<PageUp>")
+
 -- lsp
 map("n", "gr", "<cmd>Telescope lsp_references include_declaration=false<cr>")
