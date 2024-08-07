134008b264f219c7898f689a278541c29e1e9d53
dragove <dragove@qq.com>
2024-08-07 11:54:03 +0800

    nvim: delete biome

--- nvim/.config/nvim/lua/plugins/lang/core.lua
+++ nvim/.config/nvim/lua/plugins/lang/core.lua
@@ -89,7 +89,6 @@ return {
 
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'biome',
       })
       require('mason-tool-installer').setup({ ensure_installed = ensure_installed })
 
