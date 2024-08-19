f037b00769ecf2caba4f04408c905b568662dd51
dragove <dragove@qq.com>
2024-08-19 20:22:01 +0800

    nvim: add ts based comment

--- nvim/.config/nvim/lua/plugins/code/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/code/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   'nvim-treesitter/nvim-treesitter',
-  event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' },
+  event = { 'BufReadPost', 'BufNewFile' },
   config = function()
     require('nvim-treesitter.configs').setup({
       sync_install = false,
