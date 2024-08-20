54cd02ab2f749d6ffbb5cc473b44bafc09913be7
dragove <dragove@qq.com>
2024-08-20 16:50:16 +0800

    nvim: fix java load time:

--- nvim/.config/nvim/lua/plugins/lang/java.lua
+++ nvim/.config/nvim/lua/plugins/lang/java.lua
@@ -1,6 +1,17 @@
 return {
   'nvim-java/nvim-java',
   ft = { 'java' },
+  dependencies = {
+    'nvim-java/lua-async-await',
+    'nvim-java/nvim-java-refactor',
+    'nvim-java/nvim-java-core',
+    'nvim-java/nvim-java-test',
+    'nvim-java/nvim-java-dap',
+    'MunifTanjim/nui.nvim',
+    'neovim/nvim-lspconfig',
+    'mfussenegger/nvim-dap',
+    'williamboman/mason.nvim',
+  },
   config = function()
     require('java').setup({})
     require('lspconfig').jdtls.setup({})
