89bf333699db5ec0f5b9feb192b88cc7bff4892f
dragove <dragove@qq.com>
2024-09-03 23:20:12 +0800

    feat: diasble default m key

--- nvim/.config/nvim/lua/plugins/editor/arrow.lua
+++ nvim/.config/nvim/lua/plugins/editor/arrow.lua
@@ -1,6 +1,8 @@
 return {
   'otavioschwanck/arrow.nvim',
   opts = function()
+    -- disable keymap for default mark to prevent delay on m key
+    vim.keymap.set({ 'n', 'v' }, 'm', '<nop>')
     vim.keymap.set('n', 'H', require('arrow.persist').previous)
     vim.keymap.set('n', 'L', require('arrow.persist').next)
     return {
