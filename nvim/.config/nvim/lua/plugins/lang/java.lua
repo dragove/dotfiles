b99ac3750b0c08734eaf42d744c73a338653279b
dragove <dragove@qq.com>
2024-10-06 20:42:43 +0800

    java

--- nvim/.config/nvim/lua/plugins/lang/java.lua
+++ nvim/.config/nvim/lua/plugins/lang/java.lua
@@ -3,7 +3,7 @@ return {
   ft = { 'java' },
   config = function()
     local config = {
-      cmd = { '/path/to/jdt-language-server/bin/jdtls' },
+      cmd = { 'jdtls' },
       root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
     }
     require('jdtls').start_or_attach(config)
