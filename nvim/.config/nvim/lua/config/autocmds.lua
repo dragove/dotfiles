a520f936bc54e3f136a03b15b02b9b2a74132503
dragove <dragove@qq.com>
2024-10-20 12:40:38 +0800

    feat: disable modifiable when buffer is readonly

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -2,14 +2,18 @@ local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
 autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
-  group = augroup('highlight-yank', { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
 })
 autocmd({ 'TextChanged', 'InsertLeave' }, {
   desc = 'Autosave on some actions',
-  group = augroup('autosave', { clear = true }),
   command = 'silent! wall',
   nested = true,
+})
+autocmd('BufRead', {
+  desc = 'Diable Editing Readonly files',
+  callback = function()
+    vim.bo.modifiable = not vim.bo.readonly
+  end,
 })
