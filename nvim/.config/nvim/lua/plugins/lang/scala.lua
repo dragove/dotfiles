4d6f9baf5b8be405c3f7c45488b5b60135f06c97
dragove <dragove@qq.com>
2024-11-19 10:46:01 +0800

    metals: update inlay hints

--- nvim/.config/nvim/lua/plugins/lang/scala.lua
+++ nvim/.config/nvim/lua/plugins/lang/scala.lua
@@ -10,12 +10,20 @@ return {
   config = function()
     local metals_config = require("metals").bare_config()
     metals_config.settings = {
+      useGlobalExecutable = true,
       showImplicitArguments = false,
       showImplicitConversionsAndClasses = false,
       showInferredType = true,
       superMethodLensesEnabled = false,
       autoImportBuild = "initial",
       serverProperties = { "-Dmetals.enable-best-effort=true" },
+      inlayHints = {
+        hintsInPatternMatch = { enable = true },
+        implicitArguments = { enable = false },
+        implicitConversions = { enable = false },
+        inferredTypes = { enable = true },
+        typeParameters = { enable = false },
+      },
     }
     metals_config.init_options.statusBarProvider = "off"
     local capabilities = vim.lsp.protocol.make_client_capabilities()
