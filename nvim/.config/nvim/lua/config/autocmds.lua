c30edda91fee58e68ff798b06faa9a5450c7e6f6
dragove <dragove@qq.com>
2024-12-09 21:29:41 +0800

    nvim: add bigfile type

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -17,3 +17,19 @@ autocmd("BufRead", {
     vim.bo.modifiable = not vim.bo.readonly
   end,
 })
+local max_filesize = 1024 * 1024 -- 1 MB
+local function big_file(buf)
+  local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+  if ok and stats and stats.size > max_filesize then
+    return true
+  end
+end
+vim.filetype.add({
+  pattern = {
+    [".*"] = {
+      function(path, buf)
+        return path and big_file(buf) and "bigfile" or nil
+      end,
+    },
+  },
+})
--- nvim/.config/nvim/lua/plugins/code/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/code/treesitter.lua
@@ -24,14 +24,7 @@ return {
         "typescript",
       },
       highlight = {
-        enable = true,
-        disable = function(_, buf)
-          local max_filesize = 200 * 1024 -- 200 KB
-          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-          if ok and stats and stats.size > max_filesize then
-            return true
-          end
-        end,
+        enable = true
       },
     })
   end,
