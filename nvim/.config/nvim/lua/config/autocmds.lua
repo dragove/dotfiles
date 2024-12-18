f32a1c359b066ea937fdb9706efb61f23959e853
dragove <dragove@qq.com>
2024-12-18 18:05:26 +0800

    nvim: replace loop to uv

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -19,7 +19,7 @@ autocmd("BufRead", {
 })
 local max_filesize = 1024 * 1024 -- 1 MB
 local function big_file(buf)
-  local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+  local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
   if ok and stats and stats.size > max_filesize then
     return true
   end
