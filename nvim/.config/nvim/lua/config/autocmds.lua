0505281855db84b730bd93bb5bbe54f06cb52f7b
dragove <dragove@qq.com>
2025-01-20 22:22:55 +0800

    nvim: add support to fill background colors

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -40,3 +40,21 @@ vim.filetype.add({
     },
   },
 })
+-- auto fill background color on margins
+local modified = false
+autocmd({ "UIEnter", "ColorScheme" }, {
+  callback = function()
+    local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
+    if normal.bg then
+      io.write(string.format("\027]11;#%06x\027\\", normal.bg))
+      modified = true
+    end
+  end,
+})
+autocmd("UILeave", {
+  callback = function()
+    if modified then
+      io.write("\027]111\027\\")
+    end
+  end,
+})
