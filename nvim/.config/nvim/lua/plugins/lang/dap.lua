7d2df38df634ea455e890ba5649d2e35c66f857b
dragove <dragove@qq.com>
2025-03-09 00:43:28 +0800

    nvim: dap virt text

--- nvim/.config/nvim/lua/plugins/lang/dap.lua
+++ nvim/.config/nvim/lua/plugins/lang/dap.lua
@@ -1,7 +1,29 @@
 return {
   "mfussenegger/nvim-dap",
+  dependencies = {
+    "nvim-dap-virtual-text",
+  },
   config = function()
     local dap = require("dap")
+    dap.adapters.lldb = {
+      type = "executable",
+      command = "lldb-dap",
+      name = "lldb",
+    }
+    dap.configurations.cpp = {
+      {
+        name = "Launch",
+        type = "lldb",
+        request = "launch",
+        program = function()
+          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        end,
+        cwd = "${workspaceFolder}",
+        stopOnEntry = false,
+        args = {},
+      },
+    }
+    dap.configurations.c = dap.configurations.cpp
     dap.configurations.scala = {
       {
         type = "scala",
@@ -20,6 +42,17 @@ return {
         },
       },
     }
+
+    require("nvim-dap-virtual-text").setup({
+      clear_on_continue = true,
+      display_callback = function(variable)
+        if #variable.value > 15 then
+          return " " .. variable.value:sub(1, 15) .. "..."
+        end
+        return " " .. variable.value
+      end,
+    })
+
     local map = vim.keymap.set
     map("n", "<leader>dc", function()
       require("dap").continue()
