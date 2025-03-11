9b524d7bf15a85a04d914b2754037f8a66dff48f
dragove <dragove@qq.com>
2025-03-11 14:33:53 +0800

    neovim: simplify config

--- nvim/.config/nvim/lua/plugins/editor/noice.lua
+++ nvim/.config/nvim/lua/plugins/editor/noice.lua
@@ -4,10 +4,6 @@ return {
   opts = {
     lsp = {
       progress = { enabled = false },
-      override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
-      },
     },
     health = { checker = false },
     presets = {
